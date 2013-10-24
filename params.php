4e09c6c31ec5e1f06590035a9f46471e701cec93
Jurets <jurets75@gmail.com>
2013-10-24 10:13:07 +0300

    correct

--- params.php
+++ params.php
@@ -76,8 +76,8 @@ return array_merge(array(
         'host'=>'ssl://smtp.gmail.com',
         'port'=>'465',
         'auth'=>true,
-        'username'=>'',
-        'password'=>'',
+        'username'=>'-',
+        'password'=>'-',
         'timeout'=>500,
         'templates' => array(
            'invitation' => array(

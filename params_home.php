9dee588b8eceab9fad9f3bdea52b1028a9f3a15c
Jurets <jurets75@gmail.com>
2013-10-24 10:14:33 +0300

    correct

--- params_home.php
+++ params_home.php
@@ -53,8 +53,8 @@ return array(
         'host'=>'ssl://smtp.gmail.com',
         'port'=>'465',
         'auth'=>true,
-        'username'=>'jurets75',
-        'password'=>'steruj75',
+        'username'=>'-',
+        'password'=>'-',
         'timeout'=>500,
         'templates' => array(
            'invitation' => array(

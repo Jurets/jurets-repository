f73b113aec749ff057038fecf1ede4c1753eb262
Jurets <jurets75@gmail.com>
2013-12-12 16:05:29 +0200

    Интеркасса: + обработка после успешного завершения

--- paysuccess.php
+++ paysuccess.php
@@ -1,8 +1,22 @@
 <h1>Платёж успешно завершён!</h1>
 <?php
     DebugBreak();
-if (isset($_POST)) {
-    
-}
+    $filename = 'saveddata.json';
+    if (is_file($filename)) {
+        $data = file_get_contents($filename);
+        $data = json_decode($data, true);
+        $ik_lastpay_num = $data['ik_lastpay_num'];
+    } else {
+        $ik_lastpay_num = 0;
+        
+    }
+    $ik_lastpay_num = $ik_lastpay_num + 1;
+    $data = array('ik_lastpay_num'=>$ik_lastpay_num);
+    $data = json_encode($data);
+    file_put_contents($filename, $data);
+
+    if (!empty($_POST)) {
+        //чтото делаем
+    }
 
 ?>

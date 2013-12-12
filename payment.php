9ac9a457eb59a33a58b272446d84b9e3d3417294
Jurets <jurets75@gmail.com>
2013-12-12 12:39:27 +0200

    Внедрение Интеркассы -начало, простая форма

---
+++ payment.php
@@ -0,0 +1,20 @@
+<h1>Оплата за работу секретаря</h1>
+<p>25 USD</p>
+
+<form id="payment" name="payment" method="post" action="https://sci.interkassa.com/" enctype="utf-8">
+    <input type="hidden" name="ik_co_id" value="52a86a04bf4efcbb6efd1e6f" />
+    <input type="hidden" name="ik_pm_no" value="tc_0001" />
+    <input type="hidden" name="ik_am" value="0.1" />
+    <input type="hidden" name="ik_cur" value="USD" />
+    <input type="hidden" name="ik_desc" value="оплата работы секретаря" />
+    <input type="hidden" name="ik_suc_u" value="http://tkd-card.com.ua/paysuccess.php" />
+    <input type="hidden" name="ik_suc_m" value="post" />
+    <input type="hidden" name="ik_fal_u" value="http://tkd-card.com.ua/payfail.php" />
+    <input type="hidden" name="ik_fal_m" value="post" />
+    <!--<input type="hidden" name="ik_pnd_u" value="http://tkd-card.com.ua/paypendinf" />
+    <input type="hidden" name="ik_pnd_m" value="post" />-->
+    <input type="hidden" name="ik_exp" value="2013-12-15" />
+    <input type="hidden" name="ik_loc" value="ru" />
+    <input type="hidden" name="ik_enc" value="utf-8" />
+    <input type="submit" value="Оплатить">
+</form>

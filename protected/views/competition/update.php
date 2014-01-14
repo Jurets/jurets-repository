b4ad9b18a2999478454226f6dc7554ead7910789
Jurets <jurets75@gmail.com>
2014-01-14 23:36:58 +0200

    ред-ие гл. стр.

--- protected/views/competition/update.php
+++ protected/views/competition/update.php
@@ -53,7 +53,12 @@ $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
                 'minHeight'=>600,
                 'maxHeight'=>600,
                 'linebreaks'=>true,
-                'allowedTags'=>array('p', 'div', 'a', 'br', 'img', 'h1'),
+                'allowedTags'=>array(
+                    'p', 'div', 'a', 'br', 'img', 
+                    'h1', 'h2', 'h3', 
+                    'ul', 'ol', 'li', 
+                    'b', 'i', 'u', 'strike',
+                ),
             )
         ), true);
         $tabs[] = array('label'=>'Главная страница', 'content'=>$content, 'active'=>false); 

8ac889779e752430083f182c1b13f7bad1c2325a
Jurets <jurets75@gmail.com>
2014-02-12 14:22:57 +0200

    результаты ХТЗ-2014

--- protected/views/weightcategory/results.php
+++ protected/views/weightcategory/results.php
@@ -11,28 +11,27 @@ $this->breadcrumbs=array(
 
 <p><?php echo Yii::t('fullnames', 'This page will be published results of the championship')?></p>
 
-<!--<p>Чтобы скачать необходимый протокол - выберите тип документа (Распаровка или Результат), затем нужную возрастную категорию и кликните по соответствующей ссылке ниже</p>
+<p>Чтобы скачать необходимый протокол - выберите тип документа (Распаровка или Результат), затем нужную возрастную категорию и кликните по соответствующей ссылке ниже</p>
 
 <p>Распаровки:</p>
-<a id="all-download" href="<?php echo $docpath.'t_child.pdf'?>" target="_blank" style="color: maroon; ">Дети (2003 - 2005 г.р.) категория А</a>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_toss_CHILD.pdf'?>" target="_blank" style="color: maroon; ">Дети (2004 - 2006 г.р.) категория А</a>
 <br><br>
-<a id="all-download" href="<?php echo $docpath.'t_young.pdf'?>" target="_blank" style="color: maroon; ">Юноши (2001 - 2003 г.р.)</a>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_toss_YOUNG.pdf'?>" target="_blank" style="color: maroon; ">Юноши (2002 - 2004 г.р.)</a>
 <br><br>
-<a id="all-download" href="<?php echo $docpath.'t_cadet.pdf'?>" target="_blank" style="color: maroon; ">Кадеты (1999 - 2001 г.р.)</a>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_toss_CADET.pdf'?>" target="_blank" style="color: maroon; ">Кадеты (2000 - 2002 г.р.)</a>
 <br><br>
-<a id="all-download" href="<?php echo $docpath.'t_junior.pdf'?>" target="_blank" style="color: maroon; ">Юниоры (1996 - 1999 г.р.)</a>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_toss_JUNIOR.pdf'?>" target="_blank" style="color: maroon; ">Юниоры (1997 - 2000 г.р.)</a>
 <br><br>
 
 <p>Результаты:</p>
-<a id="all-download" href="<?php echo $docpath.'r_child.pdf'?>" target="_blank" style="color: maroon; ">Дети (2003 - 2005 г.р.) категория А</a>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_CHILD.pdf'?>" target="_blank" style="color: maroon; ">Дети (2004 - 2006 г.р.) категория А</a>
 <br><br>
-<a id="all-download" href="<?php echo $docpath.'r_young.pdf'?>" target="_blank" style="color: maroon; ">Юноши (2001 - 2003 г.р.)</a>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_YOUNG.pdf'?>" target="_blank" style="color: maroon; ">Юноши (2002 - 2004 г.р.)</a>
 <br><br>
-<a id="all-download" href="<?php echo $docpath.'r_cadet.pdf'?>" target="_blank" style="color: maroon; ">Кадеты (1999 - 2001 г.р.)</a>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_CADET.pdf'?>" target="_blank" style="color: maroon; ">Кадеты (2000 - 2002 г.р.)</a>
 <br><br>
-<a id="all-download" href="<?php echo $docpath.'r_junior.pdf'?>" target="_blank" style="color: maroon; ">Юниоры (1996 - 1999 г.р.)</a>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_JUNIOR.pdf'?>" target="_blank" style="color: maroon; ">Юниоры (1997 - 2000 г.р.)</a>
 <br><br>
 
 <p>Дополнительно:</p>
 <a id="all-download" href="<?php echo $docpath.'teampoints.pdf'?>" target="_blank" style="color: maroon; ">Командные результаты</a>
--->

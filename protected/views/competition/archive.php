a5b975e9b03cf401b28f62f012053668af5993fd
Jurets <jurets75@gmail.com>
2014-04-09 15:06:42 +0300

    + турнир на стр архива

--- protected/views/competition/archive.php
+++ protected/views/competition/archive.php
@@ -38,4 +38,28 @@ $this->breadcrumbs=array(
 <a id="all-download" href="<?php echo $docpath.'IF2014-junior_result.pdf'?>" target="_blank" style="color: maroon; ">Юніори (1997 - 2000 р.н.)</a>
 <br>
 <a id="all-download" href="<?php echo $docpath.'IF2014-senior_toss.pdf'?>" target="_blank" style="color: maroon; ">Молодь (1994 - 1998 р.н.)</a>
+<br><br>
+
+<h2>Чемпіонат КДЮСШ «ХТЗ» 2014 (Харьков)</h2>
+
+<p>Распаровки:</p>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_toss_CHILD.pdf'?>" target="_blank" style="color: maroon; ">Дети (2004 - 2006 г.р.) категория А</a>
 <br>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_toss_YOUNG.pdf'?>" target="_blank" style="color: maroon; ">Юноши (2002 - 2004 г.р.)</a>
+<br>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_toss_CADET.pdf'?>" target="_blank" style="color: maroon; ">Кадеты (2000 - 2002 г.р.)</a>
+<br>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_toss_JUNIOR.pdf'?>" target="_blank" style="color: maroon; ">Юниоры (1997 - 2000 г.р.)</a>
+<br><br>
+
+<p>Результаты:</p>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_CHILD.pdf'?>" target="_blank" style="color: maroon; ">Дети (2004 - 2006 г.р.) категория А</a>
+<br>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_YOUNG.pdf'?>" target="_blank" style="color: maroon; ">Юноши (2002 - 2004 г.р.)</a>
+<br>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_CADET.pdf'?>" target="_blank" style="color: maroon; ">Кадеты (2000 - 2002 г.р.)</a>
+<br>
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_JUNIOR.pdf'?>" target="_blank" style="color: maroon; ">Юниоры (1997 - 2000 г.р.)</a>
+<br><br>
+
+<a id="all-download" href="<?php echo $docpath.'HTZ2014_result_COMMAND.pdf'?>" target="_blank" style="color: maroon; ">Командные результаты</a>

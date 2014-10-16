291fd45ccf8c7b9f177ec62429fb945cfc80316f
Jurets <jurets75@gmail.com>
2014-10-16 10:06:19 +0300

    отсылка мейла (новое расширение) об автогенераци пароля (шаблон)

---
+++ protected/views/mail/autopassword.php
@@ -0,0 +1,19 @@
+<?php $this->renderPartial('application.views.mail.hello'); ?>
+
+<p style="color: #000; font-size: medium;">
+    Для Вас автоматически сгенерирован новый пароль<br>
+    Ваши Регистрационные данные:<br>
+</p>
+<hr>
+<p style="color: #000; font-size: medium;">
+    Логин: <?=$user->Email?><br>
+    Пароль: <?=$user->new_password?>
+</p>
+<hr>
+<p style="color: #000; font-size: medium;">
+    Проверьте ещё раз эти данные, и в случае необходимости свяжитесь с организаторами.
+    Вы можете авторизоваться на сайте, используя логин и пароль, указанные выше.
+    После авторизации вы сможете сменить пароль в Кабинете.
+</p>
+
+<?php $this->renderPartial('application.views.mail.regards'); ?>

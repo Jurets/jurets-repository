d1f4cf7fabd7f226090d352ea7ecedd2b766241f
Jurets <jurets75@gmail.com>
2014-10-28 12:31:47 +0200

    мерефа в архив соревнований

--- protected/views/competition/archive.php
+++ protected/views/competition/archive.php
@@ -14,18 +14,18 @@ $this->breadcrumbs=array(
 
 <h3>Відкритий чемпіонат Мереф'янської ДЮСШ Харківської районної ради Харківської області з тхеквондо (ВТФ)</h3>
 <p>Розпаровки</p>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_restoss_child(2007).pdf'?>" target="_blank" style="color: maroon; ">Діти (2007 г.р.)</a><br>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_restoss_smyoung(2005-2006).pdf'?>" target="_blank" style="color: maroon; ">Молодші юнаки (2005 - 2006 р.н.)</a> <br>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_restoss_young(2003-2004).pdf'?>" target="_blank" style="color: maroon; ">Юнаки (2003 - 2004 р.н.)</a> <br>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_restoss_cadet(2000-2002).pdf'?>" target="_blank" style="color: maroon; ">Кадети (2000 - 2002 р.н.)</a><br>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_restoss_junior(1997-1999).pdf'?>" target="_blank" style="color: maroon; ">Юніори (1997 - 1999 р.н.)</a><br><br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_restoss_child(2007).pdf'?>" target="_blank" style="color: maroon; ">Діти (2007 г.р.)</a><br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_restoss_smyoung(2005-2006).pdf'?>" target="_blank" style="color: maroon; ">Молодші юнаки (2005 - 2006 р.н.)</a> <br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_restoss_young(2003-2004).pdf'?>" target="_blank" style="color: maroon; ">Юнаки (2003 - 2004 р.н.)</a> <br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_restoss_cadet(2000-2002).pdf'?>" target="_blank" style="color: maroon; ">Кадети (2000 - 2002 р.н.)</a><br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_restoss_junior(1997-1999).pdf'?>" target="_blank" style="color: maroon; ">Юніори (1997 - 1999 р.н.)</a><br><br>
 
 <p>Результати</p>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_res_child(2007).pdf'?>" target="_blank" style="color: maroon; ">Діти (2007 г.р.)</a><br>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_res_smyoung(2005-2006).pdf'?>" target="_blank" style="color: maroon; ">Молодші юнаки (2005 - 2006 р.н.)</a><br>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_res_young(2003-2004).pdf'?>" target="_blank" style="color: maroon; ">Юнаки (2003 - 2004 р.н.)</a><br>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_res_cadet(2000-2002).pdf'?>" target="_blank" style="color: maroon; ">Кадети (2000 - 2002 р.н.)</a><br>
-<a id="all-download" href="<?php echo $docpath.'merefa2014sep_res_junior(1997-1999).pdf'?>" target="_blank" style="color: maroon; ">Юніори (1997 - 1999 р.н.)</a><br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_res_child(2007).pdf'?>" target="_blank" style="color: maroon; ">Діти (2007 г.р.)</a><br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_res_smyoung(2005-2006).pdf'?>" target="_blank" style="color: maroon; ">Молодші юнаки (2005 - 2006 р.н.)</a><br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_res_young(2003-2004).pdf'?>" target="_blank" style="color: maroon; ">Юнаки (2003 - 2004 р.н.)</a><br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_res_cadet(2000-2002).pdf'?>" target="_blank" style="color: maroon; ">Кадети (2000 - 2002 р.н.)</a><br>
+<a id="all-download" href="<?php echo $docpath.'merefa2014_res_junior(1997-1999).pdf'?>" target="_blank" style="color: maroon; ">Юніори (1997 - 1999 р.н.)</a><br>
 
 
 <h3>Открытый кубок КЗ КДЮСШ «ХТЗ» ХОР 2014, сентябрь (Харьков)</h3>

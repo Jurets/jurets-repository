e4bcda0387f5e6d8aed9789592d104968122ba99
Jurets <jurets75@gmail.com>
2015-04-29 16:49:26 +0300

    весовые (ч.3)

---
+++ protected/views/weightcategory/create.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $this AgecategoryController */
+/* @var $model Agecategory */
+
+$this->breadcrumbs=array(
+	'Weightcategories'=>array('index'),
+	'Create',
+);
+
+$this->menu=array(
+	array('label'=>'List Agecategory', 'url'=>array('index')),
+	array('label'=>'Manage Agecategory', 'url'=>array('admin')),
+);
+?>
+
+<h1>Создать весовую категорию</h1>
+
+<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>

efb0b32e30535fe356535840d50b9308b5b8f11d
Jurets <jurets75@gmail.com>
2015-09-21 17:04:25 +0300

    + ссылка на старую арх. ленту (иконка для закрытия)

--- protected/views/competition/invitation.php
+++ protected/views/competition/invitation.php
@@ -15,12 +15,11 @@
     $this->widget('bootstrap.widgets.TbAlert', array(
             'block'=>true, // display a larger alert block?
             'fade'=>true, // use transitions?
-            'closeText'=>'&times;', // close link text - if set to false, no close link is displayed
             'alerts'=>array( // configurations per alert type
-                'success'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), // success, info, warning, error or danger
-                'info'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), // success, info, warning, error or danger
-                'warning'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), // success, info, warning, error or danger
-                'error'=>array('block'=>true, 'fade'=>true, 'closeText'=>'&times;'), // success, info, warning, error or danger
+                'success'=>array('block'=>true, 'fade'=>true), // success, info, warning, error or danger
+                'info'=>array('block'=>true, 'fade'=>true), // success, info, warning, error or danger
+                'warning'=>array('block'=>true, 'fade'=>true), // success, info, warning, error or danger
+                'error'=>array('block'=>true, 'fade'=>true), // success, info, warning, error or danger
             ),
     )); 
 

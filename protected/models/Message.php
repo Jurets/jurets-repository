8abadabc3f5c91f5c40e509042f0a28999953c13
Jurets <jurets75@gmail.com>
2016-02-02 10:11:37 +0200

    * виджет для показа флэш сообщений, fix

---
+++ protected/models/Message.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * This is the model class for table "message".
+ *
+ * The followings are the available columns in table 'message':
+ * @property integer $id
+ * @property integer $competitionid
+ * @property string $category
+ * @property string $page
+ * @property string $text
+ *
+ * The followings are the available model relations:
+ * @property Competition $competition
+ */
+class Message extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'message';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('category, text', 'required'),
+			array('competitionid', 'numerical', 'integerOnly'=>true),
+			array('category, page, text', 'length', 'max'=>255),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, competitionid, category, page, text', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'competition' => array(self::BELONGS_TO, 'Competition', 'competitionid'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'competitionid' => 'Competitionid',
+			'category' => 'категория: success, error, warning',
+			'page' => 'имя страницы: {all}, command, competition, invite',
+			'text' => 'Text',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('competitionid',$this->competitionid);
+		$criteria->compare('category',$this->category,true);
+		$criteria->compare('page',$this->page,true);
+		$criteria->compare('text',$this->text,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Message the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+    
+    public function defaultScope() {
+        return array(
+            'alias'=>'message',
+            'condition'=>'status = :status',
+            'params'=>array(':status'=>1),
+        );
+    }
+}
